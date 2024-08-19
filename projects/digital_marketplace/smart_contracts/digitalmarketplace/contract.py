b84578bf284146fdc503c734ad99eb4452b1a125
Evert Diaz <evertdiazb@gmail.com>
2024-08-19 10:12:44 -0600

    Project initialised with AlgoKit CLI using template: https://github.com/algorandfoundation/algokit-python-template.git

---
+++ projects/digital_marketplace/smart_contracts/digitalmarketplace/contract.py
@@ -0,0 +1,8 @@
+from algopy import ARC4Contract, String
+from algopy.arc4 import abimethod
+
+
+class Digitalmarketplace(ARC4Contract):
+    @abimethod()
+    def hello(self, name: String) -> String:
+        return "Hello, " + name
